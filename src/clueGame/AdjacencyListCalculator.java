379d978acc3e85a015b9aba1d905ed4c42070727
Dawson Matthews <=>
2023-03-27 16:26:51 -0600

    Make the String 'Walkway' a constant.

--- src/clueGame/AdjacencyListCalculator.java
+++ src/clueGame/AdjacencyListCalculator.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 
+	private static final String WALKWAY_STRING = "Walkway";
+	
 	public static void SetAdjacencyList(int rows, int columns, BoardCell[][] griddy, Map<Character, Room> roomMap) {
 		
 		ClearAllCellAdjacencyLists(rows, columns, griddy);
@@ -13,7 +15,7 @@ public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 				
 				BoardCell cell = griddy[i][j];
 				
-				if (roomMap.get(cell.getInitial()).getName().equals("Walkway")) {
+				if (roomMap.get(cell.getInitial()).getName().equals(WALKWAY_STRING)) {
 					SetWalkwayAdjacency(rows, columns, griddy, roomMap, i, j, cell);
 				}
 				
@@ -36,7 +38,7 @@ public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 		startCenterCell.addAdjacency(targetCenterCell);
 	}
 
-	private static void SetWalkwayAdjacency(int ROWS, int COLUMNS, BoardCell[][] griddy, Map<Character, Room> roomMap, int i, int j, BoardCell cell) {
+	private static void SetWalkwayAdjacency(int rows, int columns, BoardCell[][] griddy, Map<Character, Room> roomMap, int i, int j, BoardCell cell) {
 		
 		BoardCell adjacentCell;
 		if (cell.isDoorway() == true) {
@@ -60,10 +62,10 @@ public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 			cell.addAdjacency(roomCenterCell);
 			roomCenterCell.addAdjacency(cell);
 		}
-		if ((i+1) < ROWS) {
+		if ((i+1) < rows) {
 			adjacentCell = griddy[i+1][j];
 			
-			if (roomMap.get(adjacentCell.getInitial()).getName().equals("Walkway") && adjacentCell.getOccupied() == false) {
+			if (roomMap.get(adjacentCell.getInitial()).getName().equals(WALKWAY_STRING) && adjacentCell.getOccupied() == false) {
 				cell.addAdjacency(adjacentCell);
 			}
 		}
@@ -71,15 +73,15 @@ public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 		if ((i-1) >= 0) {
 			adjacentCell = griddy[i-1][j];
 			
-			if (roomMap.get(adjacentCell.getInitial()).getName().equals("Walkway") && adjacentCell.getOccupied() == false) {
+			if (roomMap.get(adjacentCell.getInitial()).getName().equals(WALKWAY_STRING) && adjacentCell.getOccupied() == false) {
 				cell.addAdjacency(adjacentCell);
 			}
 		}
 		
-		if ((j+1) < COLUMNS) {
+		if ((j+1) < columns) {
 			adjacentCell = griddy[i][j+1];
 			
-			if (roomMap.get(adjacentCell.getInitial()).getName().equals("Walkway") && adjacentCell.getOccupied() == false) {
+			if (roomMap.get(adjacentCell.getInitial()).getName().equals(WALKWAY_STRING) && adjacentCell.getOccupied() == false) {
 				cell.addAdjacency(adjacentCell);
 			}
 		}
@@ -87,15 +89,15 @@ public class AdjacencyListCalculator implements SetWalkwayAdjacency {
 		if ((j-1) >= 0) {
 			adjacentCell = griddy[i][j-1];
 			
-			if (roomMap.get(adjacentCell.getInitial()).getName().equals("Walkway") && adjacentCell.getOccupied() == false) {
+			if (roomMap.get(adjacentCell.getInitial()).getName().equals(WALKWAY_STRING) && adjacentCell.getOccupied() == false) {
 				cell.addAdjacency(adjacentCell);
 			}
 		}
 	}
 
-	private static void ClearAllCellAdjacencyLists(int ROWS, int COLUMNS, BoardCell[][] griddy) {
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < COLUMNS; j++) {
+	private static void ClearAllCellAdjacencyLists(int rows, int columns, BoardCell[][] griddy) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
 				BoardCell cell = griddy[i][j];
 				cell.clearAdjacencyList();
 			}
